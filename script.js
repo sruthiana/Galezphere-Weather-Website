ba217a9b12fabc98001627218a44acc178a2efd2
Lakshmi <94792644+sruthiana@users.noreply.github.com>
2025-04-21 10:35:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function getCityCoordinates(){
     let cityName = cityInput.value.trim();
     cityInput.value = '';
     if(!cityName) return;
-    let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},
+    let GEOCODING_API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},
     &limit=1&appid=${api_key}`;
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
         let {name, lat, lon, country, state} = data[0];
